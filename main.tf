31712271fb09573ba831b90ccc3a14b4e671dd73
Alexander Laskavtsev <las@agrain.ua>
2024-05-10 14:00:22 +0000

    IT WORKS!

--- main.tf
+++ main.tf
@@ -1,3 +1,31 @@
 module "kind_cluster" {
   source = "github.com/den-vasyliev/tf-kind-cluster?ref=cert_auth"
 }
+
+module "github_repository" {
+  source                   = "github.com/laskavtsev-dev/tf-github-repository"
+  github_owner             = var.GITHUB_OWNER
+  github_token             = var.GITHUB_TOKEN
+  repository_name          = var.FLUX_GITHUB_REPO
+  public_key_openssh       = module.tls_private_key.public_key_openssh
+  public_key_openssh_title = "flux"
+}
+
+module "flux_bootstrap" {
+  source            = "github.com/laskavtsev-dev/tf-fluxcd-flux-bootstrap"
+  github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
+  private_key       = module.tls_private_key.private_key_pem
+  config_path       = module.gke_cluster.kubeconfig
+}
+
+module "tls_private_key" {
+  source = "github.com/laskavtsev-dev/tf-hashicorp-tls-keys"
+  algorithm = "RSA"
+}
+
+module "gke_cluster" {
+  source         = "github.com/laskavtsev-dev/tf-google-gke-cluster"
+  GOOGLE_REGION  = var.GOOGLE_REGION
+  GOOGLE_PROJECT = var.GOOGLE_PROJECT
+  GKE_NUM_NODES  = 1
+}
--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "GOOGLE_PROJECT" {
   type        = string
-  description = "GCP project name"
+  description = "GCP project to use"
 }
 
 variable "GOOGLE_REGION" {
@@ -9,26 +9,24 @@ variable "GOOGLE_REGION" {
   description = "GCP region to use"
 }
 
-variable "GKE_MACHINE_TYPE" {
+variable "GITHUB_OWNER" {
   type        = string
-  default     = "g1-small"
-  description = "Machine type"
+  description = "GitHub owner repository to use"
 }
 
-variable "GKE_NUM_NODES" {
-  type        = number
-  default     = 2
-  description = "GKE nodes number"
+variable "GITHUB_TOKEN" {
+  type        = string
+  description = "GitHub personal access token"
 }
 
-variable "GKE_CLUSTER_NAME" {
+variable "FLUX_GITHUB_REPO" {
   type        = string
-  default     = "main"
-  description = "GKE cluster name"
+  default     = "flux-gitops"
+  description = "Flux GitOps repository"
 }
 
-variable "GKE_POOL_NAME" {
+variable "FLUX_GITHUB_TARGET_PATH" {
   type        = string
-  default     = "main"
-  description = "GKE pool name"
+  default     = "clusters"
+  description = "Flux manifests subdirectory"
 }
